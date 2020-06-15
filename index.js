30612050384f187a5ed288af4c715e1ff0012c18
SimpleaRU <info@simplea.ru>
2020-06-15 11:10:10 +0300

    add scripts defer, template xml

--- index.js
+++ index.js
@@ -1,47 +1,26 @@
-function dynamicallyLoadScript(src) {
-	return new Promise((resolve, reject) => {
-		const script = document.createElement('script');
-		script.src = src;
-		// script.onreadystatechange = callback; // For old ie 
-		document.body.appendChild(script);
+const downloadXML = document.getElementById("downloadXML");
+const unloader = xmlParser();
+const store = mainStore();
+const serialize = new XMLSerializer();
+store.gatherFields("myForm");
+const emptyXMLTemplate =
+  '<?xml version="1.0" encoding="Windows-1251"?><root></root>';
 
-		script.onload = () => {
-			resolve();
-		};
+unloader.initialize("fileChooser", store.putToStore.bind(store));
 
-		script.onerror = (error) => {
-			reject(error);
-		}
-	})
-};
+downloadXML.addEventListener("click", (event) => {
+  const domXML = unloader.createXML(emptyXMLTemplate, store.getFromStore());
+  console.log("domXML ", domXML);
 
-// Main thread
-Promise.all([
-	dynamicallyLoadScript('./scripts/xmlParser.js'),
-	dynamicallyLoadScript('./scripts/store.js'),
-])
-	.then(() => {
-		const downloadXML = document.getElementById('downloadXML');
-		const unloader = xmlParser();
-		const store = mainStore();
-		const serialize = new XMLSerializer();
-		store.gatherFields('myForm');
-		const emptyXMLTemplate = '<?xml version="1.0" encoding="Windows-1251"?><root></root>'
+  const bb = new Blob([serialize.serializeToString(domXML)], {
+    type: "text/plain",
+  });
 
-		unloader.initialize('fileChooser', store.putToStore.bind(store));
-// Download handler
-		downloadXML.addEventListener('click', (event) => {
-			const domXML = unloader.createXML(emptyXMLTemplate, store.getFromStore());
-			console.log('domXML ', domXML);
-
-			const bb = new Blob([serialize.serializeToString(domXML)], { type: 'text/plain' });
-
-			downloadXML.setAttribute('href', window.URL.createObjectURL(bb));
-			downloadXML.setAttribute('download', 'file.xml');
-			downloadXML.dataset.downloadurl = ['text/plain', downloadXML.download, downloadXML.href].join(':');
-		});
-
-	})
-	.catch((error) => {
-		console.log(error);
-	});
+  downloadXML.setAttribute("href", window.URL.createObjectURL(bb));
+  downloadXML.setAttribute("download", "file.xml");
+  downloadXML.dataset.downloadurl = [
+    "text/plain",
+    downloadXML.download,
+    downloadXML.href,
+  ].join(":");
+});
